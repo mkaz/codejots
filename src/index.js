ccd711910e2541c8b7cf28950b2950e738a02214
Marcus Kazmierczak <marcus@mkaz.com>
2018-07-09 07:27:15 -0700

    Add redux tools extension

--- src/index.js
+++ src/index.js
@@ -5,15 +5,17 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { middleware as reduxPackMiddleware } from 'redux-pack';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 /* Internal */
 import './assets/index.scss';
 import App from './App';
 import reducers from './store/reducers';
 
+
 const store = createStore( 
     reducers,
-    applyMiddleware( reduxPackMiddleware )
+    composeWithDevTools( applyMiddleware( reduxPackMiddleware ) ),
 );
 
 ReactDOM.render(
